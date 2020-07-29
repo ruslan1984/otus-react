ded2dfdbb3832d1fc66555a264d0384d6e475814
Ruslan <ruslan231984@yandex.ru>
2020-07-29 14:56:09 +0300

    hw2

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: require.resolve("babel-loader"),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
