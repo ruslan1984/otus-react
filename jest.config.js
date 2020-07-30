b517d888d5d367299960ff2865436df18eda012f
Ruslan <ruslan231984@yandex.ru>
2020-07-31 02:15:24 +0300

    config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  }
+};
