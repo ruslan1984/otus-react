acabc5c4bf0608529bd6a30fcd61f5bf13c775dc
Ruslan <ruslan231984@yandex.ru>
2020-08-27 21:51:58 +0300

    hw7

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require( "path" );
+const webpackRules = require( "./webpackRules" );
+
+const HtmlWebpackPlugin = require( "html-webpack-plugin" );
+const PrettierPlugin = require("prettier-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  mode: "development",
+  resolve: {
+    extensions: [ ".js", ".jsx", ".ts", ".tsx", ".json" ],
+  },
+  output: {
+    path: path.join( __dirname, "/dist" ),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/i,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader",
+            options: {
+              modules: true }
+          },
+      ]
+      },
+      // {
+      //   test: /\.(js|ts)x?$/,
+      //   exclude: /node_modules/,
+      //   use: ['eslint-loader']
+      // },
+      ...webpackRules,
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin( {
+      template: "./src/index.html",
+    } ),
+    new PrettierPlugin({
+      printWidth: 80,
+      tabWidth: 4
+  })
+  ],
+};
