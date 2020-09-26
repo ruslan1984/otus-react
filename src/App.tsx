3c56bbf3b7aa1ee9480f425c27da991a87ffe1a9
Ruslan <ruslan231984@yandex.ru>
2020-09-26 15:33:52 +0300

    fix. Route to self file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import React, { FC } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import AuthPage from "@auth/AuthPage";
-import AdminAppHoc from "@admin/AdminAppHoc";
+import { AppRoute } from "./AppRoute";
 
 const App: FC = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={AuthPage} />
-                <Route exact path="/auth" component={AuthPage} />
-                <Route exact path="/admin" component={AdminAppHoc} />
-                <Route path="*" render={() => 404} />
-            </Switch>
-        </BrowserRouter>
-    );
+    return <AppRoute />;
 };
 
 export default App;
