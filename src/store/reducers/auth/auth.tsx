af0f01df75211c7f2daa799f91e931c4791389da
Ruslan <ruslan231984@yandex.ru>
2020-09-27 21:19:12 +0300

    return store

--- src/store/reducers/auth/auth.tsx
+++ src/store/reducers/auth/auth.tsx
@@ -1,26 +1,25 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { auth } from "@auth/types";
-import { CheckState } from "@auth/types";
+import { auth, CheckState } from "@auth/types";
 
 const defaultState: auth = {
-    loading: false,
-    status: CheckState.initiated,
+  loading: false,
+  status: CheckState.initiated,
 };
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState: defaultState,
-    reducers: {
-        auth: (state, action: PayloadAction<CheckState>) => {
-            return { ...state, status: action.payload };
-        },
-        login: (state, { payload }: PayloadAction<any>) => {
-            return { ...state };
-        },
-        logout: () => {},
-        loading: (state, action: PayloadAction<boolean>) => {
-            return { ...state, loading: action.payload };
-        },
-    },
+  name: "auth",
+  initialState: defaultState,
+  reducers: {
+    auth: (state, action: PayloadAction<CheckState>) => ({
+      ...state,
+      status: action.payload,
+    }),
+    login: (state) => state,
+    logout: (state) => state,
+    loading: (state, action: PayloadAction<boolean>) => ({
+      ...state,
+      loading: action.payload,
+    }),
+  },
 });
 export const { actions, reducer } = authSlice;
